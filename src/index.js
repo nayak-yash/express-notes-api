f3c3ecb05a40f3971658605a17f28065e6a9c04d
Yash Kumar <97407569+nayak-yash@users.noreply.github.com>
2022-12-29 14:06:56 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -22,7 +22,7 @@ app.get("/", (req, res) =>{
 
 const PORT = process.env.PORT || 5000;
 const MONGO_URL=process.env.MONGO_URL;
-console.out(MONGO_URL);
+console.log(MONGO_URL);
 mongoose.set("strictQuery", false);
 mongoose.connect(MONGO_URL)
 .then(()=>{
