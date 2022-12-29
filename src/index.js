96f4d0bf55859f889e15e9f58af51a1d1403ee0b
Yash Kumar <97407569+nayak-yash@users.noreply.github.com>
2022-12-29 16:56:54 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,7 +17,7 @@ app.use("/users", userRouter);
 app.use("/note", noteRouter);
 
 app.get("/", (req, res) =>{
-    res.send("Notes API From CheezyCode");
+    res.send("Notes API From Express");
 });
 
 const PORT = process.env.PORT || 5000;
