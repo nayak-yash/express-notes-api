a064fc59baf08848f5943ea0a7297e02a5115544
Yash Kumar <97407569+nayak-yash@users.noreply.github.com>
2023-01-06 11:01:09 +0530

    Update noteController.js

--- src/controllers/noteController.js
+++ src/controllers/noteController.js
@@ -1,35 +1,40 @@
 const noteModel = require("../models/note");
 
 const createNote = async (req, res) =>{
-    
-    const {title, description} = req.body;
+    const {title, description,createdAt,location,status,isPrivate,isFavorite} = req.body;
 
     const newNote = new noteModel({
         title: title,
         description : description,
-        userId : req.userId
+        userId : req.userId,
+        createdAt : createdAt,
+        location : location,
+        isFavorite : isFavorite,
+        status : status,
+        isPrivate : isPrivate
     });
-
     try {
-        
         await newNote.save();
         res.status(201).json(newNote);
-
     } catch (error) {
         console.log(error);
         res.status(500).json({message: "Something went wrong"});
     }
-    
 }
 
 const updateNote = async (req, res) =>{
     const id = req.params.id;
-    const {title, description} = req.body;
+    const {title, description,createdAt,location,status,isPrivate,isFavorite} = req.body;
 
     const newNote = {
-        title : title,
-        description: description,
-        userId : req.userId
+        title: title,
+        description : description,
+        userId : req.userId,
+        createdAt : createdAt,
+        location : location,
+        isFavorite : isFavorite,
+        status : status,
+        isPrivate : isPrivate
     }
 
     try {
